02029b252e277ef8a554224d087a2bf1b5fdc35a
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-02-26 05:39:50 +0700

    feat: added meshsdk, eslint, and TS

--- frontend/electrust/next.config.mjs
+++ frontend/electrust/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ['images.unsplash.com'],
+    },
+    webpack: function (config, options) {
+        config.experiments = {
+            asyncWebAssembly: true,
+            layers: true,
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
