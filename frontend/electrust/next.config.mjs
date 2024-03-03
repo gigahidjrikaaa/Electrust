eadc3b217496539d69133d534279d67af6b7033f
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-03-04 00:36:26 +0700

    fix: reinit project

--- frontend/electrust/next.config.mjs
+++ frontend/electrust/next.config.mjs
@@ -1,13 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: function (config, options) {
-    config.experiments = {
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
