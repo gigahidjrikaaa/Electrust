483f91fa870e6aa2827697276d52c7d044dd94d7
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-03-14 11:45:13 +0700

    feat: moved hero to a component

--- frontend/electrust/src/pages/index.tsx
+++ frontend/electrust/src/pages/index.tsx
@@ -6,7 +6,7 @@ import Navbar from "@/components/navbar";
 import { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import { Button } from "@nextui-org/button";
+import Hero from "@/components/landingpage/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,16 +14,7 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div data-aos="fade-down">     
-        <div className="h-screen flex flex-col justify-center items-center text-justify mt-20">
-          <div className="w-56 h-56 border-white border-2 mb-4 rounded-full overflow-hidden">
-            <Image src="/electrust-logo.svg" className="w-full h-full justify-center text-center" alt="Electrust Logo" width={100} height={100} />
-          </div>
-          <h1 className="font-bold font-russo-one text-6xl">Electrust</h1>
-          <p className="font-sans text-2xl">Secure, Transparent, Accessible.</p>      
-          <Button color="primary" className="my-10 w-fit text-xl px-20 py-8 font-russo-one">Get Started</Button>
-        </div>
-      </div>
+      <Hero />
       <div data-aos="fade-down" className="bg-oxfordBlue w-full">
         <div className="h-screen flex flex-col justify-center items-center text-justify mt-20">
         </div>
