5a90f461d9aebf726ac0cadaf2570b3c7c828468
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-04-01 23:37:14 +0700

    feat: added Linkedin, Twt, and Instagram on Footer

--- frontend/electrust/src/components/footer/index.tsx
+++ frontend/electrust/src/components/footer/index.tsx
@@ -9,6 +9,18 @@ const Footer: React.FC = () => {
                         <p className="text-center">
                             &copy; 2024 threesixty - Senpro Team 5. All Rights Reserved.
                         </p>
+                        {/* Linkedin, Twitter, and instagram logo */}
+                        <div className="d-flex justify-content-center">
+                            <a href="https://www.linkedin.com/">
+                                <i className="fab fa-linkedin fa-2x text-white"></i>
+                            </a>
+                            <a href="https://www.twitter.com/">
+                                <i className="fab fa-twitter fa-2x text-white"></i>
+                            </a>
+                            <a href="https://www.instagram.com/">
+                                <i className="fab fa-instagram fa-2x text-white"></i>
+                            </a>
+                        </div>                        
                     </div>
                 </div>
             </div>
