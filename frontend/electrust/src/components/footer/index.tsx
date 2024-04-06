f44a2e08170e690cc4fdf0fe972ce2f0a8738370
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-04-07 00:17:47 +0700

    feat: footer & error page fix

--- frontend/electrust/src/components/footer/index.tsx
+++ frontend/electrust/src/components/footer/index.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
     return (
-        <footer>
-            <div className="container py-5 bg-yaleBlue w-screen">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <p className="text-center">
-                            &copy; 2024 threesixty - Senpro Team 5. All Rights Reserved.
-                        </p>
-                        {/* Linkedin, Twitter, and instagram logo */}
-                        <div className="d-flex justify-content-center">
-                            <a href="https://www.linkedin.com/">
-                                <i className="fab fa-linkedin fa-2x text-white"></i>
-                            </a>
-                            <a href="https://www.twitter.com/">
-                                <i className="fab fa-twitter fa-2x text-white"></i>
-                            </a>
-                            <a href="https://www.instagram.com/">
-                                <i className="fab fa-instagram fa-2x text-white"></i>
-                            </a>
-                        </div>                        
-                    </div>
+        <footer className='bg-oxfordBlue py-5 hover:bg-mikadoYellow hover:text-oxfordBlue transition-all'>
+            <div className="row">
+                <div className="flex flex-col">
+                    {/* Linkedin, Twitter, and instagram logo */}
+                    <div className="flex justify-center items-center text-justify">
+                        <a href="https://www.linkedin.com/">
+                            <Image src="/linkedin.svg" alt="Linkedin Logo" width={30} height={30} />
+                        </a>
+                        <a href="https://www.twitter.com/">
+                            <Image src="/twitter.svg" alt="Twitter Logo" width={30} height={30} />
+                        </a>
+                        <a href="https://www.instagram.com/">
+                            <Image src="/instagram.svg" alt="Instagram Logo" width={30} height={30} />
+                        </a>
+                    </div>  
+                    <p className="text-center justify-center items-center">
+                        &copy; 2024 threesixty - Senpro Team 5. All Rights Reserved.
+                    </p>                    
                 </div>
             </div>
         </footer>
--- frontend/electrust/src/pages/error/index.tsx
+++ frontend/electrust/src/pages/error/index.tsx
@@ -1,12 +1,20 @@
-import { Button, Spacer } from '@nextui-org/react';
+"use client";
+import React from 'react';
+import Footer from '@/components/footer';
+import Navbar from '@/components/navbar';
+import { Button } from '@nextui-org/button';
+
 
 const ErrorPage: React.FC = () => {
     return (
         <div>
-            <Spacer y={2} />
-            <h1>Error 404</h1>
-            <Spacer y={1} />
-            <p>Page not found</p>
+            <Navbar />
+            <div className="bg-black h-screen w-full mt-0 flex flex-col justify-center items-center text-justify">
+                <h1 className='text-5xl pt-10 text-mikadoYellow font-russo-one'>Error 404</h1>
+                <p className='text-xl py-5'>Page not found.</p>
+                <Button color='primary' className='hover:bg-mikadoYellow hover:text-oxfordBlue w-1/5 py-10 mb-8 mt-4 text-3xl font-bold' onClick={() => window.location.href = '/'}>Go Back</Button>
+            </div>
+            <Footer />
         </div>
     );
 };
