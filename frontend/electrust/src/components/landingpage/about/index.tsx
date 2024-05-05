4bbf04f57b5a14eedb7c8be5c4388a4678e5c7ad
krisnaarianta <krisnaarianta@gmail.com>
2024-05-06 00:09:53 +0700

    fix: about page

--- frontend/electrust/src/components/landingpage/about/index.tsx
+++ frontend/electrust/src/components/landingpage/about/index.tsx
@@ -7,7 +7,7 @@ export default function About() {
       <div data-aos="fade-down" className="text-white">
         <h1 className="font-bold font-russo-one text-6xl">About</h1>
         <div className="font-sans text-2xl text-center max-w-md">
-          <p>Welcome to Electrust! Electrust is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.</p>
+          <p>Welcome to Electrust! This platform is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.</p>
           <p>Our mission is to ensure the integrity and security of your data while providing a seamless user experience.</p>
           <p>Join us on our journey to revolutionize trust in the digital world!</p>
         </div>
