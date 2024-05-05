302a4c09f7fa258717373e9bee1a1b49afcdee0d
krisnaarianta <krisnaarianta@gmail.com>
2024-05-06 00:14:22 +0700

    fix: add navbar to the about page

--- frontend/electrust/src/components/landingpage/about/index.tsx
+++ frontend/electrust/src/components/landingpage/about/index.tsx
@@ -1,15 +1,20 @@
 "use client";
+import Navbar from '@/components/navbar';
+
 import "aos/dist/aos.css";
 
 export default function About() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center text-justify mt-20 bg-gray-100">
-      <div data-aos="fade-down" className="text-white">
-        <h1 className="font-bold font-russo-one text-6xl">About</h1>
-        <div className="font-sans text-2xl text-center max-w-md">
-          <p>Welcome to Electrust! This platform is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.</p>
-          <p>Our mission is to ensure the integrity and security of your data while providing a seamless user experience.</p>
-          <p>Join us on our journey to revolutionize trust in the digital world!</p>
+    <div>
+      <Navbar />
+      <div className="h-screen flex flex-col justify-center items-center text-justify mt-20 bg-gray-100">
+        <div data-aos="fade-down" className="text-white">
+          <h1 className="font-bold font-russo-one text-6xl">About</h1>
+          <div className="font-sans text-2xl text-center max-w-md">
+            <p>Welcome to Electrust! This platform is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.</p>
+            <p>Our mission is to ensure the integrity and security of your data while providing a seamless user experience.</p>
+            <p>Join us on our journey to revolutionize trust in the digital world!</p>
+          </div>
         </div>
       </div>
     </div>
