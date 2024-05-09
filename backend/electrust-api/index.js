097e7c15e2efb2fd49a5a1a84ade5bb43136289b
gigahidjrikaaa <gigahidjrikaaa@gmail.com>
2024-05-09 23:15:08 +0700

    feat: backend status page

--- backend/electrust-api/index.js
+++ backend/electrust-api/index.js
@@ -3,6 +3,12 @@ const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => {
+    // redirect to the status-page
+    res.redirect('/status');
+    }
+);
+
+app.get('/status', (req, res) => {
     // show the status-page from the public folder
     res.sendFile(__dirname + '/public/status-page/index.html');
     }
