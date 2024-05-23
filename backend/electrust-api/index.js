4d487d289fde332fae02d235690cef8628ed9547
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-05-23 18:07:48 +0700

    feat: removed secrets

--- backend/electrust-api/index.js
+++ backend/electrust-api/index.js
@@ -48,6 +48,3 @@ app.get('/status', (req, res) => {
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-// ENV
-// mongodb+srv://<username>:<password>@maincluster.cokesvs.mongodb.net/?retryWrites=true&w=majority&appName=MainCluster
