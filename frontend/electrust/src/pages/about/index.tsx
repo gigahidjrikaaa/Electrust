ca5f318a768abfd14ab1dbc72bb07b401bb5514f
krisnaarianta <krisnaarianta@gmail.com>
2024-05-23 18:30:50 +0700

    fix: about page

--- frontend/electrust/src/pages/about/index.tsx
+++ frontend/electrust/src/pages/about/index.tsx
@@ -17,9 +17,9 @@ export default function About() {
           <div data-aos="fade-down" className="text-white">
             <h1 className="text-4xl font-bold font-russo-one text-mikadoYellow">About</h1>
             <div className="text-white text-center max-w-md mt-8">
-              <p>Welcome to Electrust! This platform is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.</p>
-              <p>Our mission is to ensure the integrity and security of your data while providing a seamless user experience.</p>
-              <p>Join us on our journey to revolutionize trust in the digital world!</p>
+              <p>Welcome to Electrust! This platform is a secure, transparent, and accessible application that aims to provide trustworthy voting services to its users.
+              Our mission is to ensure the integrity and security of your data while providing a seamless user experience.
+              Join us on our journey to revolutionize trust in the digital world!</p>
             </div>
           </div>
         </div>
@@ -29,15 +29,18 @@ export default function About() {
           <div className="flex justify-center items-center space-x-8">
             <div className="text-center transform rotate-[-5deg] hover:rotate-0 transition duration-300">
               <Image src="/cand1.jpg" alt="Developer 1" width={200} height={200} className="rounded-full" />
-              <p className="mt-4 font-semibold">Giga Hidjrika Aura Adkhy</p>
+              <p className="mt-4 font-semibold text-oxfordBlue">Giga Hidjrika Aura Adkhy</p>
+              <p className="font-semibold text-oxfordBlue">21/473893/TK/52247</p>
             </div>
             <div className="text-center transform rotate-[5deg] hover:rotate-0 transition duration-300">
               <Image src="/cand1.jpg" alt="Developer 2" width={200} height={200} className="rounded-full" />
-              <p className="mt-4 font-semibold">Ahmad Fauzan</p>
+              <p className="mt-4 font-semibold text-oxfordBlue">Ahmad Fauzan</p>
+              <p className="font-semibold text-oxfordBlue">21/473893/TK/52247</p>
             </div>
             <div className="text-center transform rotate-[-5deg] hover:rotate-0 transition duration-300">
-              <Image src="/cand1.jpg" alt="Developer 3" width={200} height={200} className="rounded-full" />
-              <p className="mt-4 font-semibold">Johanes de Britto Krisna Arianta</p>
+              <Image src="/fotokrisna.jpg" alt="Developer 3" width={200} height={200} className="rounded-full" />
+              <p className="mt-4 font-semibold text-oxfordBlue">Johanes de Britto Krisna Arianta</p>
+              <p className="font-semibold text-oxfordBlue">21/473893/TK/52247</p>
             </div>
           </div>
         </div>
