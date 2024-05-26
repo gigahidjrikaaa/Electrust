7214a80523e4e1d5f6602d466d934c7c2113b71c
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-05-27 02:01:11 +0700

    feat: added userController

---
+++ backend/electrust-api/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require('../models/User');
+
+const getUser = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        res.status(500).send('Server error');
+    }
+};
+
+module.exports = { getUser };
