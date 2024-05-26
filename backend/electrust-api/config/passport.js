9b8ab9f7c4674e1145aca9c292ea97324823a81a
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-05-27 02:05:15 +0700

    feat: refactored passport.js

--- backend/electrust-api/config/passport.js
+++ backend/electrust-api/config/passport.js
@@ -1,48 +1,86 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
 const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
-// Local strategy
-passport.use(new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-    User.findOne({ email }, async (err, user) => {
-        if (err) return done(err);
-        if (!user) return done(null, false, { message: 'No user found' });
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) return done(null, false, { message: 'Password incorrect' });
-
-        return done(null, user);
-    });
-}));
-
-// Google strategy
-passport.use(new GoogleStrategy({
-    clientID: process.env.GOOGLE_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'https://localhost:4000/auth/google/callback'
-}, async (token, tokenSecret, profile, done) => {
-    const newUser = {
-        googleId: profile.id,
-        name: profile.displayName,
-        email: profile.emails[0].value
-    };
+passport.use(
+  new LocalStrategy({ usernameField: 'email' }, async (email, password, done) => {
     try {
-        let user = await User.findOne({ googleId: profile.id });
-        if (user) {
-            done(null, user);
-        } else {
-            user = await User.create(newUser);
-            done(null, user);
-        }
+      const user = await User.findOne({ email });
+      if (!user) return done(null, false, { message: 'Invalid credentials' });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return done(null, false, { message: 'Invalid credentials' });
+
+      return done(null, user);
     } catch (err) {
-        done(err, false);
+      return done(err);
     }
-}));
+  })
+);
 
-passport.serializeUser((user, done) => done(null, user.id));
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/api/auth/google/callback',
+    },
+    async (token, tokenSecret, profile, done) => {
+      try {
+        const user = await User.findOne({ googleId: profile.id });
+        if (user) {
+          return done(null, user);
+        }
+        const newUser = new User({
+          googleId: profile.id,
+          name: profile.displayName,
+          email: profile.emails[0].value,
+        });
+        await newUser.save();
+        return done(null, newUser);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
 
-passport.deserializeUser((id, done) => {
-    User.findById(id, (err, user) => done(err, user));
+passport.use(
+  new JwtStrategy(
+    {
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    },
+    async (jwtPayload, done) => {
+      try {
+        const user = await User.findById(jwtPayload.user.id);
+        if (user) {
+          return done(null, user);
+        } else {
+          return done(null, false);
+        }
+      } catch (err) {
+        return done(err, false);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
 });
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+module.exports = passport;
