0f37d1243093bac9846d69635ea4d9a6be4ac1d7
AhmadFauzan19 <68361425+AhmadFauzan19@users.noreply.github.com>
2024-05-27 04:57:00 +0700

    fix: googel auth

--- backend/electrust-api/index.js
+++ backend/electrust-api/index.js
@@ -1,43 +1,42 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
-const passport = require('./config/passport');
+const passport = require('passport');
 const authRoutes = require('./routes/authRoutes');
 const electionRoutes = require('./routes/electionRoutes');
 const userRoutes = require('./routes/userRoutes');
 const session = require('express-session');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
 const app = express();
 
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-app.use(passport.initialize());
-
 
 // Configure session middleware
 app.use(session({
   secret: 'your-secret-key',
   resave: false,
   saveUninitialized: true,
+  cookie: { secure: false } // Set secure to true if using HTTPS
 }));
 
+// Initialize Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/elections', electionRoutes);
 app.use('/api/user', userRoutes);
-
-// Initialize Passport
-app.use(passport.initialize());
-app.use(passport.session());
 
 // Configure Passport to use Google OAuth 2.0 strategy
 passport.use(new GoogleStrategy({
