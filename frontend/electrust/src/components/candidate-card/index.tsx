5412bfff0649ff8be1a3dbe22aaeec97791e21ef
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-05-27 09:33:13 +0700

    feat: UI fix

--- frontend/electrust/src/components/candidate-card/index.tsx
+++ frontend/electrust/src/components/candidate-card/index.tsx
@@ -21,14 +21,14 @@ export default function CandidateCard({ name, image, voteID }: { name: string, i
 
     return (
         <div className="my-8">
-            <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="4xl">
+            <Modal isOpen={isOpen} onOpenChange={onOpenChange} size="4xl" className="max-h-unit-9xl">
                 <ModalContent>
-                    <ModalHeader>{name}</ModalHeader>
+                    <ModalHeader className="justify-center text-center font-russo-one text-3xl">{name}</ModalHeader>
                     <ModalBody>
-                        <div className="flex flex-row">
-                            <Image src={image} width={200} height={200} className="w-1/2 rounded-xl" alt={""}/>
-                            <div className="flex flex-col mx-6">
-                                <h2 className="text-2xl font-bold font-russo-one text-justify">Candidate Information</h2>
+                        <div className="flex flex-row overflow-hidden max-h-unit-7xl">
+                            <Image src={image} width={200} height={100} className="w-full h-auto aspect-auto rounded-xl" alt={""}/>
+                            <div className="flex flex-col mx-6 overflow-auto">
+                                <h2 className="text-2xl font-bold font-russo-one text-justify my-3">Candidate Information</h2>
                                 <p>
                                     Sunt ad dolore quis aute consequat. Magna exercitation reprehenderit
                                     magna aute tempor cupidatat consequat elit dolor adipisicing. Mollit
@@ -36,6 +36,28 @@ export default function CandidateCard({ name, image, voteID }: { name: string, i
                                     eiusmod Lorem aliqua enim laboris do dolor eiusmod. Et mollit
                                     incididunt nisi consectetur esse laborum eiusmod pariatur proident
                                     Lorem eiusmod et. Culpa deserunt nostrud ad veniam.
+                                </p>
+                                <h2 className="text-2xl font-bold font-russo-one text-justify my-3">Political Party</h2>
+                                <p>
+                                    Sit nulla est ex deserunt exercitation anim occaecat. Nostrud
+                                    ullamco deserunt aute id consequat veniam incididunt duis in sint
+                                    irure nisi.
+                                </p>
+                                <h2 className="text-2xl font-bold font-russo-one text-justify my-3">Vision</h2>
+                                <p>
+                                    <ul>
+                                        <li>First Vision</li>
+                                        <li>Second Vision</li>
+                                        <li>Third Vision</li>
+                                    </ul>
+                                </p>
+                                <h2 className="text-2xl font-bold font-russo-one text-justify my-3">Mission</h2>
+                                <p>
+                                    <ul>
+                                        <li>First Mission</li>
+                                        <li>Second Mission</li>
+                                        <li>Third Mission</li>
+                                    </ul>
                                 </p>
                             </div>
                         </div>
