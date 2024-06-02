4d2e902e511cb85f6baa716128e7c4dcf5d27eed
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-06-02 17:44:04 +0700

    feat: update auth.js to handle roles

--- backend/electrust-api/middlewares/auth.js
+++ backend/electrust-api/middlewares/auth.js
@@ -1,5 +1,31 @@
-const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const ensureAuthenticated = passport.authenticate('jwt', { session: false });
+const auth = (roles = []) => {
+  if (typeof roles === 'string') {
+    roles = [roles];
+  }
 
-module.exports = { ensureAuthenticated };
+  return [
+    (req, res, next) => {
+      const token = req.header('Authorization').replace('Bearer ', '');
+      if (!token) return res.status(401).send('Access Denied');
+      try {
+        const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+        req.user = verified;
+        next();
+      } catch (err) {
+        res.status(400).send('Invalid Token');
+      }
+    },
+    async (req, res, next) => {
+      const user = await User.findById(req.user._id);
+      if (!user || (roles.length && !roles.includes(user.role))) {
+        return res.status(403).send('Access Denied');
+      }
+      next();
+    }
+  ];
+};
+
+module.exports = auth;
