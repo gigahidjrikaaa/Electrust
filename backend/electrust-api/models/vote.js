1e6244ed84464a0254bcc9dfc4f13054ccfdbe38
krisnaarianta <krisnaarianta@gmail.com>
2024-06-02 18:18:50 +0700

    fix: vote

--- backend/electrust-api/models/vote.js
+++ backend/electrust-api/models/vote.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 
 const VoteSchema = new mongoose.Schema({
-    electionId: mongoose.Schema.Types.ObjectId,
-    candidate: String,
-    userId: String
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    electionId: { type: mongoose.Schema.Types.ObjectId, ref: 'Election', required: true },
+    candidateId: { type: mongoose.Schema.Types.ObjectId, ref: 'Candidate', required: true },
 });
 
 module.exports = mongoose.model('Vote', VoteSchema);
