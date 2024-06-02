ad7f1b7744467b1c5439264aaa6990cb4fb04f21
krisnaarianta <krisnaarianta@gmail.com>
2024-06-02 19:00:35 +0700

    fix: protect vote routes

--- backend/electrust-api/routes/voteRoutes.js
+++ backend/electrust-api/routes/voteRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const voteController = require('../controllers/voteController');
+const auth = require('../middlewares/auth');
 
-router.post('/', voteController.createVote);
-router.get('/', voteController.getVotes);
+router.post('/:electionId/vote', auth(['voter']), voteController.submitVote);
+router.get('/:electionId/results', auth(['admin', 'developer']), voteController.getResults);
 
 module.exports = router;
