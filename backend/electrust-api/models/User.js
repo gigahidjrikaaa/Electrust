2ce48350afedc89ccd3582a5fda7ffefdab61406
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-06-03 03:08:41 +0700

    feat: added username & role to userSchema

--- backend/electrust-api/models/User.js
+++ backend/electrust-api/models/User.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String },
     googleId: { type: String },
+    role: { type: String, enum: ['voter', 'admin', 'developer'], default: 'voter'},
     date: { type: Date, default: Date.now }
 });
+
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+UserSchema.methods.matchPassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
