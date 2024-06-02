11dd8a2fdedc022a88e5e496c8927fd5ac09e60c
Giga Hidjrika Aura Adkhy <52191436+gigahidjrikaaa@users.noreply.github.com>
2024-06-02 15:12:36 +0700

    fix: eslint and babel

--- frontend/electrust/next.config.mjs
+++ frontend/electrust/next.config.mjs
@@ -3,10 +3,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // pageExtensions:
-  // {
-  //   webpack:true
-  // }
+  webpack: function (config, options) {
+    config.experiments = {
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
